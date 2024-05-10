ee02b79ff727b5d752e1cb8ea518ed795a4c079a
AmitAvital1 <116808245+AmitAvital1@users.noreply.github.com>
2024-05-10 18:51:26 +0300

    Update PrintUtil.java

--- PrintUtil.java
+++ PrintUtil.java
@@ -1,7 +1,7 @@
 public class PrintUtil{
 	public static void main(String[] args){
 		for(int i = 1; i <= 10; i++){
-			System.out.println("Amit Avital");
+			System.out.println("Amit Avital Print Num " + i);
 		}
 	}
 }
